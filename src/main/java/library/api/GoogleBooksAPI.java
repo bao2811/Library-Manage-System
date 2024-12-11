18ebf83ae86007a4bf8eb8840b0091eaed527601
hoangbao <158168159+bao2811@users.noreply.github.com>
2024-12-11 22:51:53 +0700

    Update GoogleBooksAPI.java

--- src/main/java/library/api/GoogleBooksAPI.java
+++ src/main/java/library/api/GoogleBooksAPI.java
@@ -11,7 +11,7 @@ import okhttp3.Response;
 
 public class GoogleBooksAPI {
   private final OkHttpClient client = new OkHttpClient();
-  private static final String API_KEY = "AIzaSyBO9hgSSlQiaHH_agblC5W8W_afy9rugtA"; // Sử dụng API Key của bạn
+  private static final String API_KEY = ""; // Sử dụng API Key của bạn
 
   public String searchBook(String query) throws IOException {
     String url = "https://www.googleapis.com/books/v1/volumes?q=" + query + "&maxResults=40&key=" + API_KEY;
